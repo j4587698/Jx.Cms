b6292037a87e86d82e870b9132c3b57f35d916e7
jx <jx@jvxiang.com>
2025-10-11 14:14:42 +0800

    修改step

---
+++ src/core/Jx.Cms.Install/StepBase.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Components;
+
+namespace Jx.Cms.Install.Components;
+
+public class StepBase: ComponentBase
+{
+    [Parameter]
+    public Action Prev { get; set; }
+
+    [Parameter]
+    public Action Next { get; set; }
+}
